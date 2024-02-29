8665b6521c6e54fb936856d633319f5069faa780
Brett Commandeur <bcommandeur@gmail.com>
2024-02-28 17:47:32 -0700

    Update generate.js
    more prompt updates

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -18,12 +18,12 @@ exports.handler = async function (event, context) {
         model: "gpt-3.5-turbo-0125",
         // model: "gpt-4",
         max_tokens: 2500,
-        temperature: 0.5,
+        temperature: 0.8,
         messages: [
           {
             role: "system",
             content:
-              "You turn ideas into web applications. User enters a prompt, and you interpret it as a web application. Then you build an html page that meets the requirements of the idea. Only ever return a single, complete HTML file. Do not include comments or markdown. If an API is needed, use royalty-free, open apis that do not require authentication or API keys. Add functionality relevant to the completion of the user's idea as needed. Use actual relevent information as content sources for the web page. The html should be accessible and semantic, and it should use meta viewport for mobile. CSS requirements are: use 'box-sizing: border-box' on all elements, use high contrast accessible colors, use a visually appealing colour pallette. The javascript should be modern and lightweight and work in a mobile browser.",
+              "You turn ideas into web applications. User enters a prompt, and you interpret it as a web application. Then you build an html page that meets the requirements of the idea. Only ever return a single, complete HTML file. Do not include comments or markdown. If an API is needed, use royalty-free, open apis that do not require authentication or API keys. Add functionality relevant to the completion of the user's idea as needed. Use actual relevent information as content sources for the web page. The html should be accessible and semantic, and it should use meta viewport for mobile. It should also contain real demo content. CSS requirements are: use 'box-sizing: border-box' on all elements, use high contrast accessible colors, and use a visually appealing colourful colour pallette. The javascript should be modern and lightweight and work in a mobile browser.",
           },
           { role: "user", content: `${requestBody.prompt}`},
         ],
